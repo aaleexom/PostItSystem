b97461350c19c7bf9b3d1e043fee77276efa7ea9
aaleexom <alejandroolivaresmompo@hotmail.com>
2018-03-08 15:13:43 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ $("#guineapig").click(function(){
 $("#add").click(function(){
 	title = $("#titleToAdd").val();
 	text = $("#textToAdd").val();
+	text = text.replace(/\n/g, "<br />");
 
 	addNote = "<div class=\"col-3 bg-flat2 mx-1 my-4\"><div class=\"row text-center bg-flat1\"><span class=\"wFull h5 pt-1\">" + title + "<span class=\"float-right\" id=\"deletePost\"><i class=\" mr-2 fas fa-times fa-xs deleteBottom2\"></i></span></span></div><div class=\"row text-justify\"><span class=\"px-4 py-2\">" + text + "</span></div></div>";
 	$("#inputPost").hide();
